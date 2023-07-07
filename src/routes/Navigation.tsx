98a4aa1b4c7453dff3bad54c31dc457916e8c32a
daniel tamayo <tamayodesigne@gmail.com>
2023-07-06 22:26:18 -0500

    add componnt compound

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,58 +1,46 @@
-import { Suspense } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Routes, Route, NavLink, Navigate } from 'react-router-dom';
-import { routes } from './routes';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink
+} from 'react-router-dom';
 
-import logo from '../logo.svg'
-
-
+import logo from '../logo.svg';
+import { ShoppingPage } from '../02-component-patterns/pages/ShoppingPage';
 
 export const Navigation = () => {
-    return (
-        <Suspense fallback={ <span>Loading...</span> }>
+  return (
+    <Router>
+      <div className="main-layout">
+        <nav>
+            <img src={ logo } alt="React Logo" />
+          <ul>
+            <li>
+              <NavLink to="/" activeClassName="nav-active" exact>Shopping</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" activeClassName="nav-active" exact>About</NavLink>
+            </li>
+            <li>
+              <NavLink to="/users" activeClassName="nav-active" exact>Users</NavLink>
+            </li>
+          </ul>
+        </nav>
 
-            <BrowserRouter>
-                <div className="main-layout">
-                    <nav>
-                        <img src={ logo } alt="React Logo" />
-                        <ul>
-                            
-                            {
-                                routes.map(({ to, name }) => (
-                                    <li key={ to }>
-                                        <NavLink 
-                                            to={ to } 
-                                            className={ ({ isActive }) => isActive ? 'nav-active' : '' }>
-                                            { name }
-                                        </NavLink>
-                                    </li>
-                                ))
-                                    
-                            }
-                            
-                            
-                        </ul>
-                    </nav>
-
-
-                    <Routes>
-                        
-                        {
-                            routes.map( ({ path, Component  }) => (
-                                <Route 
-                                    key={ path }
-                                    path={ path }
-                                    element={ <Component /> } 
-                                />
-                            ))
-                        }
-
-                        
-                        <Route path="/*" element={ <Navigate to={ routes[0].to } replace /> } />
-                    </Routes>
-
-                </div>
-            </BrowserRouter>
-        </Suspense>
-    )
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/about">
+            <h1>About</h1>
+          </Route>
+          <Route path="/users">
+            <h1>Users</h1>
+          </Route>
+          <Route path="/">
+            <ShoppingPage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
